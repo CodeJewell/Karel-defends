ec5a0d220023fe338c0f366d624b56daa7d4de89
Charles Scott <clscott94@gmail.com>
2021-04-14 19:59:47 -0400

    first commit

---
+++ KarelDefendsDemocracy.java
@@ -0,0 +1,42 @@
+package week1;
+import stanford.karel.*;
+
+public class KarelDefendsDemocracy extends SuperKarel {
+
+	public void run() {
+		
+		move();
+		move();
+		move();
+		move();
+		move();
+		turnLeft();
+		move();
+		pickBeeper();
+		turnLeft();
+		turnLeft();
+		move();
+		turnLeft();
+		move();
+		move();
+		move();
+		move();
+		turnLeft();
+		move();
+		pickBeeper();
+		turnLeft();
+		turnLeft();
+		move();
+		move();
+		pickBeeper();
+		pickBeeper();
+		turnLeft();
+		turnLeft();
+		move();
+		turnRight();
+		move();
+		
+	}
+	
+		
+}
